41a8f3cac3a54afad052de219076c7640ebf9e0c
wiggy <37914184+realtkco@users.noreply.github.com>
2021-03-26 17:41:25 -0500

    Update setup.js

--- commands/Verify/setup.js
+++ commands/Verify/setup.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
     EditMessage(client, '825135961276284988', new MessageEmbed() // Your Channel ID
     .setTitle('Reaction Roles')
     .setColor('RED')
-    .setDescription(`You must agree to our server rules, terms of service to continue with joining this discord server, Please react with <:Member:821224366061912074> to get the member role and continue.`)
+    .setDescription(`You must agree to our server rules, terms of service to continue with joining this discord server, Please read over <#821209731858825266> and <#821209733955715133> before continue. Please react with <:Member:821224366061912074> to get the member role and continue.`)
     .setFooter('Fornite Bot')
     , ['<:Member:821224366061912074>'] // Your Emojis
     )
