356e4c9630b52273cd6ce713cf001a2723643376
John <37914184+realtkco@users.noreply.github.com>
2021-05-16 01:46:13 -0500

    Update setup.js

--- commands/Aether-React-Role/setup.js
+++ commands/Aether-React-Role/setup.js
@@ -16,7 +16,7 @@ module.exports = (client) => {
     .setTitle('Rules')
     .setColor('RED')
     .setDescription(`The following rules are to be followed at all times during your time within our server. Breaking any of the rules may result in a warning, followed by a kick or ban depending on the severity of the issue.
-    
+
     1. Do not post Not Safe For Work stuff in any chat besides NSFW.
     If you have to question your content, do not post it. If you post Gore, You will be removed.
 
@@ -46,6 +46,22 @@ module.exports = (client) => {
 
     10. Use the language we can moderate, English
     We can only moderate the languages our moderators speak!
+
+    11. Do not sabotage any games.
+    This service is provided to help players improve and gain money. If you throw or sabotage people expierence it affects us and makes them no longer want to play here. If you wish to report this open a ticket with evidence and report a player.
+
+    12. Riot Terms Of service
+    You must follow Riot TOS at all times, Failure will result in immediate termination of your account with our service. Located at https://www.riotgames.com/en/terms-of-service (This includes asking for boosts, etc)
+
+    13. Toxicity is heavily moderated
+    We understand losing a game is very annoying, But going out of your way to curse somebody out or ruin the fun for other is unacceptable and will result in 1 warning before you being removed from our service.
+
+    14. Lying about your rank is frowned upon
+    Lying about your rank is not allow, If it is clearly a joke like "Papercup 3", It is okay then. (We are here to have fun, not to be the fun police.)
+
+    15. Staff Have Final Say
+    Staff members always have final say in situations where a punishment is involved or being told to do something (within our games).
+
 `)
     .setFooter('If you agree to the rules, Please react below.')
     , ['<:check:842221380060708886>'] // Your Emojis
